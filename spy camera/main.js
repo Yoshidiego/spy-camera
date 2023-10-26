a7dbf6919bf1449e3d5a4830bfa2c98c545b5744
Yoshidiego <146372389+Yoshidiego@users.noreply.github.com>
2023-10-26 10:49:10 -0600

    Add files via upload

--- spy camera/main.js
+++ spy camera/main.js
@@ -22,6 +22,44 @@ link.href=image;
 link.click();
 }
 
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+function start()
+{
+   document.getElementById("textbox").innerHTML = ""; 
+   recognition.start();
+} 
+recognition.onresult = function(event) {
+console.log(event); 
+var Content = event.results[0][0].transcript;
+document.getElementById("textbox").innerHTML = Content;
+console.log(Content);
+ 
+ if(Content =="toma mi selfie")
+    {
+      console.log("tomando selfie --- ");
+      speak();
+    }
+ }
 
+ function speak()
+ {
+    var synth=windows.speechSystethesis;
+    speak_data="tomando tu selfie en 5 segundos";
+    var utterThis=newSpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    webcam.attach(camera);
+    setTimeout(function(){
+        take_selfie();
+        save();
+    }, 5000);
+}
+
+function save(){
+    link=document.getElementById("link");
+    image=document.getElementById("selfie_image").src;
+    link.href=image;
+    link.click;
+}
 
 
